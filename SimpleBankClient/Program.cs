b9f208bd6cd008947676ec306a082c6aa63f202d
Matti Andreas Nielsen <matti@denlillemand.com>
2022-09-09 18:43:18 +0200

    Simple bank client example

---
+++ SimpleBankClient/Program.cs
@@ -0,0 +1,55 @@
+﻿using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using Utilities;
+using SmallBank;
+using SmallBank.Interfaces;
+using SmallBank.Grains;
+using Concurrency.Interface.Configuration;
+
+
+var client = new ClientBuilder()
+.UseLocalhostClustering()
+.Configure<ClusterOptions>(options =>
+{
+    options.ClusterId = "ec2";
+    options.ServiceId = "Snapper";
+})
+.Build();
+
+await client.Connect();
+
+
+//var globalConfigGrain = client.GetGrain<IGlobalConfigGrain>(0);
+//await globalConfigGrain.ConfigGlobalEnv();
+
+var actorId = 0;
+var actor = client.GetGrain<ISnapperTransactionalAccountGrain>(actorId);
+
+// Required for the Tx 
+Type snapperTransactionalAccountGrainType = typeof(SmallBank.Grains.SnapperTransactionalAccountGrain);
+string snapperTransactionalAccountGrainTypeName = snapperTransactionalAccountGrainType.ToString();
+
+//API we want to call:
+//Task<TransactionResult> StartTransaction(string startFunc, 
+//                                         object funcInput,
+//                                         List<int> grainAccessInfo,
+//                                         List<string> grainClassName);
+
+var actorAccessInfo = new List<int>();
+actorAccessInfo.Add(actorId); // it takes 1 call to Init to finish this tx?
+
+var grainClassName = new List<string>();                                             // grainID, grainClassName
+grainClassName.Add(snapperTransactionalAccountGrainTypeName);
+
+var initialBalance = 100;
+
+Console.WriteLine("Started deterministic tx");
+try {
+    var PACT_balance = await actor.StartTransaction("Init", initialBalance, actorAccessInfo, grainClassName);
+    Console.WriteLine("The PACT balance:"+PACT_balance);
+} catch (Exception e) {
+    Console.WriteLine(e.Message);
+}
+
+Console.WriteLine("Ended deterministic tx");
