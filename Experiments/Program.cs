6b09bb00fb3e41c5803e01d5bca546da046ad992
daniellvan <daniel@vanfamily.com>
2022-11-01 18:35:08 +0100

    refactor client

--- Experiments/Program.cs
+++ Experiments/Program.cs
@@ -17,48 +17,36 @@ namespace Experiments
             string region = args[1];
             int multitransfers = int.Parse(args[2]);
 
-            if("LocalDeployment".Equals(args[0]))
+            var client = CreateClusterClient(deploymentType);
+            ExperimentRunner experimentRunner = new ExperimentRunner();
+
+            await experimentRunner.ManyMultiTransferTransactions(client, region, multitransfers);
+        }
+
+        private static IClusterClient CreateClusterClient(string deploymentType)
+        {
+            var clientBuilder = new ClientBuilder()
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "Snapper";
+                    options.ServiceId = "Snapper";
+                })
+                .Configure<ClientMessagingOptions>(options =>
+                {
+                    options.ResponseTimeout = new TimeSpan(0, 5, 0);
+                });
+
+            if(deploymentType.Equals("LocalDeployment", StringComparison.CurrentCultureIgnoreCase))
             {
-                var client = new ClientBuilder()
-                    .Configure<ClusterOptions>(options =>
-                    {
-                        options.ClusterId = "Snapper";
-                        options.ServiceId = "Snapper";
-                    })
-                    .Configure<ClientMessagingOptions>(options =>
-                    {
-                        options.ResponseTimeout = new TimeSpan(0, 5, 0);
-                    })
-                    .UseLocalhostClustering()
-                    .Build();
-
-                ExperimentRunner experimentRunner = new ExperimentRunner();
-
-                await experimentRunner.ManyMultiTransferTransactions(client, region, multitransfers);
-
+                clientBuilder.UseLocalhostClustering();
             }
-            else
+            else if(deploymentType.Equals("GlobalDeployment", StringComparison.CurrentCultureIgnoreCase))
             {
                 const string key1 = "DefaultEndpointsProtocol=https;AccountName=snapperstorage;AccountKey=OYoqvb955xUGAu9SkZEMapbNAxl3vN3En2wNqVQV6iEmZE4UWCydMFL/cO+78QvN0ufhxWZNlZIA+AStQx1IXQ==;EndpointSuffix=core.windows.net";
-
-                var client = new ClientBuilder()
-                    .Configure<ClusterOptions>(options =>
-                    {
-                        options.ClusterId = "Snapper";
-                        options.ServiceId = "Snapper";
-                    })
-                    .Configure<ClientMessagingOptions>(options =>
-                    {
-                        options.ResponseTimeout = new TimeSpan(0, 5, 0);
-                    })
-                    .UseAzureStorageClustering(options => options.ConfigureTableServiceClient(key1))
-                    .Build();
-
-                ExperimentRunner experimentRunner = new ExperimentRunner();
-
-                await experimentRunner.ManyMultiTransferTransactions(client, region, multitransfers);
-
+                clientBuilder.UseAzureStorageClustering(options => options.ConfigureTableServiceClient(key1));
             }
+
+            return clientBuilder.Build();
         }
     }
 }
